5e469bcfc3845388cf09dcadaab50dc53573a11a
Juan Daniel Guzman <jg585480@gmail.com>
2025-10-01 12:20:57 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="min-h-screen bg-gray-100">
+    <div class="container mx-auto px-4 py-8">
+      <h1 class="text-4xl font-bold text-center text-blue-600 mb-8">
+        Welcome to Vue.js with Tailwind CSS
+      </h1>
+      <div class="bg-white shadow-lg rounded-lg p-6">
+        <p class="text-gray-700 text-lg">
+          This is a sample component using Tailwind CSS classes for styling.
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
